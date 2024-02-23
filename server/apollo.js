685200d97e55786bdb1fbc401b8449f3830b0047
ukeh <ukeh.hari@gmail.com>
2024-02-23 16:22:03 +0530

    updated

--- server/apollo.js
+++ server/apollo.js
@@ -1,17 +1,18 @@
 import { withApollo } from 'next-apollo';
 // import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { ApolloClient, InMemoryCache,ApolloLink,HttpLink, ApolloProvider, gql } from '@apollo/client';
-import { requestInterceptor,responseInterceptor,authLink } from './interceptor';
+import { ApolloClient, InMemoryCache, ApolloLink, HttpLink, ApolloProvider, gql } from '@apollo/client';
+import { requestInterceptor, responseInterceptor, authLink } from './interceptor';
 
-const API_URI = "http://localhost:3018/graphql"
+const API_URI = process.env.NEXT_PUBLIC_SERVER_URL;
 
+console.log(API_URI)
 const httpLink = new HttpLink({ uri: API_URI });
 
 
-const apolloClient = new ApolloClient( {
+const apolloClient = new ApolloClient({
     // uri:API_URI,
-    link: ApolloLink.from([authLink, responseInterceptor,httpLink]),
+    link: ApolloLink.from([authLink, responseInterceptor, httpLink]),
     cache: new InMemoryCache()
-} );
+});
 
-export default withApollo( apolloClient );
+export default withApollo(apolloClient);
