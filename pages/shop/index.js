b9499e93d3ea0151f177f71979eb218babdf75e5
shilpacredot <shilpa.s@credot.dev>
2024-02-27 22:57:28 +0530

    :

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -73,7 +73,7 @@ function Shop() {
   const [getProducts, { data, loading, error }] = useLazyQuery(GET_PRODUCTS);
   console.log(data);
   const [perPage, setPerPage] = useState(12);
-  // const [pagenumber,setPagenumber]=useState(page ?? 0)
+  
   const [sortBy, setSortBy] = useState(query.sortBy ? query.sortBy : "default");
   const products = data && data?.getProducts?.records;
   const totalPage = data
