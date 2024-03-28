82aed38f9c17d1532fcc17332b71abb0eaee266f
Muhammed Ijlan <ijlanijlu580@gmail.com>
2024-03-28 10:18:53 +0530

    updates

--- components/common/partials/cart-menu.jsx
+++ components/common/partials/cart-menu.jsx
@@ -12,8 +12,6 @@ import { AiOutlineClose } from "react-icons/ai";
 import { getCartTotal } from "../../../utils";
 import withApollo from "../../../server/apollo";
 import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
-import { EventEmitter } from "fbemitter";
-// import eventEmmitter from "../../../server/eventEmmitter";
 const GET_CART = gql`
   query GetCart {
   getCart {
